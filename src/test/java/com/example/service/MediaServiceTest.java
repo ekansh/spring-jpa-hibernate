e006ecf7ef0955fea99192cf31ba21a18efd332d
ekansh <mail.ekansh@gmail.com>
2017-12-24 00:51:09 +0000

    test cases

---
+++ src/test/java/com/example/service/MediaServiceTest.java
@@ -0,0 +1,36 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.example.config.AppConfig;
+import com.example.entity.Media;
+import com.example.entity.Video;
+
+import junit.framework.TestCase;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes={AppConfig.class})
+public class MediaServiceTest extends TestCase
+{
+	@Autowired
+	MediaService mediaService;
+	public MediaServiceTest() {
+	}
+
+   @Test
+    public void testInsertMedia(){
+    	Media m = new Video();
+		m.setName("Sample WORLD");
+		mediaService.add(m);
+		List<Media> listMedia = mediaService.listMedia();
+		for (Media media : listMedia) {
+			System.out.println("Media Name "+ media.getName());
+		}
+    }
+}
